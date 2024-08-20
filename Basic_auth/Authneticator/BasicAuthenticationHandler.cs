7d4715f62b748e6ca148f303a054728717931cb6
Harijagan <jaganhari2002@gmail.com>
2024-08-20 06:01:36 +0530

    authentication done

--- Basic_auth/Authneticator/BasicAuthenticationHandler.cs
+++ Basic_auth/Authneticator/BasicAuthenticationHandler.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text;
 using System.Text.Encodings.Web;
 
 namespace Basic_auth.Authneticator
@@ -13,7 +16,45 @@ namespace Basic_auth.Authneticator
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
 
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+
+            string authorizationHeader = Request.Headers["Authoriztion"];
+
+            if(string.IsNullOrEmpty(authorizationHeader))
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+
+            if(!authorizationHeader.StartsWith("basic ",StringComparison.OrdinalIgnoreCase))
+            {
+                return AuthenticateResult.Fail("Unauthorized");
+            }
+
+            var token = authorizationHeader.Substring(6);
+
+            var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+
+            var credentials = credentialString.Split(":");
+
+            var username = credentials[0];
+            var password = credentials[1];
+
+            if(username == "harijagan" || password == "idkPassword") {
+
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier,username),
+                };
+                var idnetity = new ClaimsIdentity(claims, "basic");
+                var claimPrincipal = new ClaimsPrincipal(idnetity);
+
+                return AuthenticateResult.Success(new AuthenticationTicket(claimPrincipal, Scheme.Name));
+
           
+            }
 
             return AuthenticateResult.Fail("Unauthorized");
         }

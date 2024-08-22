740592b5ef805b973f7c93487d4e07964f7c5891
Harijagan <jaganhari2002@gmail.com>
2024-08-22 05:41:54 +0530

    created config jwth string and registered services in program

--- jwt_auth/jwt_auth/Program.cs
+++ jwt_auth/jwt_auth/Program.cs
@@ -1,8 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Authentication
+
+builder.Services.AddAuthentication(options =>
+{
+
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(
+
+    jwtOption =>
+    {
+        var key = builder.Configuration.GetValue<string>("jwtAuth:key");
+        var keyBytes = Encoding.UTF8.GetBytes(key);
+        jwtOption.SaveToken = true;
+        jwtOption.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidAudience = "http://localhost:5257",
+            ValidIssuer = "http://localhost:5257",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            IssuerSigningKey = new SymmetricSecurityKey(keyBytes)
+        };
+    }
+ );
 
 var app = builder.Build();
 
